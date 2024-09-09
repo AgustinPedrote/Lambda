23ba7dd86ddac72e80d9b97a27b4446b518fe90e
agucadiz <agu.cadiz@hotmail.com>
2024-09-09 18:03:44 +0200

    Agregar un submenu en el sidebar

--- lambda/resources/views/layouts/includes/admin/sidebar.blade.php
+++ lambda/resources/views/layouts/includes/admin/sidebar.blade.php
@@ -1,4 +1,5 @@
 @php
+    // Array $links que contiene la información de los enlaces del sidebar
     $links = [
         [
             'name' => 'Dashboard',
@@ -16,14 +17,28 @@
             'active' => false,
         ],
         [
-            'name' => 'Portadas',
-            'icon' => 'fa-solid fa-images',
-            'route' => '',
+            'name' => 'Empresa',
+            'icon' => 'fa-solid fa-building',
             'active' => false,
+            'submenu' => [
+                [
+                    'name' => 'Información',
+                    'icon' => 'fa-regular fa-circle',
+                    'route' => '',
+                    'active' => false,
+                ],
+                [
+                    'name' => 'Información',
+                    'icon' => 'fa-regular fa-circle',
+                    'route' => '',
+                    'active' => false,
+                ],
+            ],
         ],
     ];
 @endphp
 
+<!-- Sidebar donde se mostrarán los enlaces del menú -->
 <aside id="logo-sidebar"
     class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
     :class="{
@@ -31,23 +46,68 @@
         '-translate-x-full': !open,
     }" aria-label="Sidebar">
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+        <!-- Lista de enlaces del menú -->
         <ul class="space-y-2 font-medium">
             @foreach ($links as $link)
                 <li>
+                    <!-- Verifica si el elemento es un encabezado -->
                     @isset($link['header'])
                         <div class="px-3 py-2 text-xs font-semibold text-gray-500 uppercase">
                             {{ $link['header'] }}
                         </div>
                     @else
-                        <a href="{{ $link['route'] }}"
-                            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $link['active'] ? 'bg-gray-100' : '' }}">
-                            <span class="inline-flex w-6 h-6 justify-center items-center">
-                                <i class="{{ $link['icon'] }} text-gray-500"></i>
-                            </span>
-                            <span class="ms-3">
-                                {{ $link['name'] }}
-                            </span>
-                        </a>
+                        <!-- Verifica si el enlace tiene un submenú -->
+                        @isset($link['submenu'])
+                            <div x-data="{
+                                open: {{ $link['active'] ? 'true' : 'false' }},
+                            }">
+                                <button
+                                    class="flex items-center w-full p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $link['active'] ? 'bg-gray-100' : '' }}"
+                                    x-on:click="open = !open">
+                                    <span class="inline-flex w-6 h-6 justify-center items-center">
+                                        <i class="{{ $link['icon'] }} text-gray-500"></i>
+                                    </span>
+                                    <span class="ms-3 text-left flex-1">
+                                        {{ $link['name'] }}
+                                    </span>
+
+                                    <i class="fa-solid fa-angle-down"
+                                        :class="{
+                                            'fa-angle-down': !open,
+                                            'fa-angle-up': open,
+                                        }">
+                                    </i>
+                                </button>
+
+                                <!-- Submenú que se despliega si 'open' es true -->
+                                <ul x-show="open" x-cloak>
+                                    @foreach ($link['submenu'] as $item)
+                                        <li class="pl-4">
+                                            <a href=""
+                                                class="flex items-center w-full p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $item['active'] ? 'bg-gray-100' : '' }}">
+                                                <span class="inline-flex w-6 h-6 justify-center items-center">
+                                                    <i class="{{ $item['icon'] }} text-gray-500"></i>
+                                                </span>
+                                                <span class="ms-3 text-left flex-1">
+                                                    {{ $item['name'] }}
+                                                </span>
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @else
+                            <!-- Enlace que no tiene submenú -->
+                            <a href="{{ $link['route'] }}"
+                                class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $link['active'] ? 'bg-gray-100' : '' }}">
+                                <span class="inline-flex w-6 h-6 justify-center items-center">
+                                    <i class="{{ $link['icon'] }} text-gray-500"></i>
+                                </span>
+                                <span class="ms-3">
+                                    {{ $link['name'] }}
+                                </span>
+                            </a>
+                        @endisset
                     @endisset
                 </li>
             @endforeach
