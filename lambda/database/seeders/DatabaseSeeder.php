2a686a93c632cdf91a37276ff64c93184028f169
agucadiz <agu.cadiz@hotmail.com>
2024-09-14 18:40:11 +0200

    Categorías, niveles, y precios a traves de seeders

--- lambda/database/seeders/DatabaseSeeder.php
+++ lambda/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +16,16 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Agustín Pedrote',
+            'email' => 'agustin@agustin.com',
+            'password' => bcrypt('12345678')
+        ]);
+
+        $this->call([
+            CategorySeeder::class,
+            LevelSeeder::class,
+            PriceSeeder::class,
+        ]);
     }
 }
